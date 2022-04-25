a99bf047844d2dd8b187de52e3092bafeeaf0554
Tim Crisp <timothycrisp@gmail.com>
2022-04-25 08:13:11 -0500

    dot length

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ console.log(
   typeof ['arrays', 'are', 'objects']
 )
 
-nums = [2, 4, 8]
-console.log(nums)
-
 let students = ['Fabian', 'Rohan', 'Garam', 'Tim', 'Jae', 'Erin']
 
 console.log(students)
+
+// statically retrieve an element from an array
+console.log(students[5])
+console.log(students[1][0])
+
+console.log(students.length)
