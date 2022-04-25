f1f61b9c6bb372de68b140515c8f8814f1a5b3a4
Tim Crisp <timothycrisp@gmail.com>
2022-04-25 08:02:25 -0500

    students array

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ console.log(
   typeof ['arrays', 'are', 'objects']
 )
 
+nums = [2, 4, 8]
+console.log(nums)
+
+let students = ['Fabian', 'Rohan', 'Garam', 'Tim', 'Jae', 'Erin']
+
+console.log(students)
