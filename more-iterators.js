dfa97d5653dcc907f773d34239d1144b9ff8adc5
Tim Crisp <timothycrisp@gmail.com>
2022-04-25 19:49:54 -0500

    for each practice

--- more-iterators.js
+++ more-iterators.js
@@ -1,2 +1,18 @@
 console.log('hi')
 
+const friends = ["Melissa", "Marc", "Andrew", "Nick"]
+
+friends.forEach(function(friend) {
+  console.log(`I have a friend named ${friend}`)
+})
+
+friends.forEach(function(friend) {
+  console.log('I have a friend named ' + friend)
+})
+
+let myFriend = []
+
+for (i = 0; i < friends.length; i++) {
+  myFriend.push(friends[i])
+}
+
