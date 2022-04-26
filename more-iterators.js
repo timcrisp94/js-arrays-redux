400026572b892762af9a997b36ea2e873fd5923f
Tim Crisp <timothycrisp@gmail.com>
2022-04-26 14:12:17 -0500

    find method practice

--- more-iterators.js
+++ more-iterators.js
@@ -215,3 +215,35 @@ console.log(nice)
 
 /*--- using an arrow function for the callback ---*/
 // const odds = nums.filter(num => num % 2)
+
+const cars = [
+  {color: 'red', make: 'BMW', year: 2001},
+  {color: 'white', make: 'Toyota', year: 2013},
+  {color: 'blue', make: 'Ford', year: 2014},
+  {color: 'white', make: 'Tesla', year: 2016}
+]
+
+let firstWhiteCar = cars.find(function(car) {
+  return car.color === 'white'
+});
+
+let frstWhiteCar = cars.find(function(car) {
+  return car.color === 'white'
+})
+
+let bmw = cars.find(car => car.make === 'BMW')
+console.log(bmw)
+
+let toyota = cars.find(function(car) {
+  return car.make === 'Toyota'
+})
+
+console.log(toyota)
+
+let missingCar = cars.find(function(car) {
+  return car.color === 'black'
+});
+// missingCar = undefined
+
+// /*--- using an arrow function for the callback ---*/
+// let firstWhiteCar = cars.find(car => car.color === 'white')
