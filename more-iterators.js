27e8b2ffd2354d95486de6a11deb20869fe85474
Tim Crisp <timothycrisp@gmail.com>
2022-04-26 13:01:35 -0500

    for each practice

--- more-iterators.js
+++ more-iterators.js
@@ -2,17 +2,50 @@ console.log('hi')
 
 const friends = ["Melissa", "Marc", "Andrew", "Nick"]
 
-friends.forEach(function(friend) {
-  console.log(`I have a friend named ${friend}`)
+// friends.forEach(function(friend) {
+//   console.log(`I have a friend named ${friend}`)
+// })
+
+// friends.forEach(function(friend) {
+//   console.log('I have a friend named ' + friend)
+// })
+
+// friends.forEach(function(friend) {
+//   console.log(`I have a friend named ${friend}`)
+// })
+
+// friends.forEach(function(friend) {
+//   console.log(`I have a friend named ${friend}`)
+// })
+
+// let friendNamed = []
+
+// for (let i = 0; i < friends.length; i++) {
+//   console.log(friends[i])
+//   friendNamed.push('I have a friend named ' + friends[i])
+// }
+
+// console.log(friendNamed)
+
+// friends.forEach(function(friend) {
+//   console.log(friend.toLowerCase())
+// })
+
+// friends.forEach(function(friend) {
+//   console.log(friend.toLowerCase())
+// })
+
+// friends.forEach(function(friend) {
+//   console.log(friend.toLowerCase())
+// })
+
+// friends.forEach(function(friend) {
+//   console.log(friend.toUpperCase())
+// })
+
+const nums = [1, 2, 3]
+const squared = nums.map(function(num) {
+  return num * num
 })
 
-friends.forEach(function(friend) {
-  console.log('I have a friend named ' + friend)
-})
-
-let myFriend = []
-
-for (i = 0; i < friends.length; i++) {
-  myFriend.push(friends[i])
-}
-
+console.log(squared)
